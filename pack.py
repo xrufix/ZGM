c31ebc78dfbf54734e56b57d6ff75493f480acba
rufix <stefan@ruhfass.com>
2018-02-21 19:23:20 +0100

    Automated packing

---
+++ pack.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+import subprocess
+import sys
+
+gitDir = os.path.dirname(os.path.realpath(__file__))
+tempDir = gitDir + "\\_temp"
+missionDir = gitDir + "\\xru_ZGM"
+mapsDir = gitDir + "\\maps"
+
+print("file:       " + __file__)
+print("gitDir:     " + gitDir)
+print("tempDir:    " + tempDir)
+print("missionDir: " + missionDir)
+print("mapsDir:    " + mapsDir)
+
+def createTemp():
+    global tempDir, gitDir
+    os.makedirs(tempDir)
+    for root, dirs, files in os.walk(missionDir):
+        if "\\." not in root:
+            for name in dirs:
+                if "." not in name:
+                    newDir = tempDir + os.path.join(root, name)[len(missionDir):]
+                    os.makedirs(newDir)
+            for name in files:
+                if name not in ["mission.sqm", "default_loadouts.hpp"]:
+                    target = tempDir + root[len(missionDir):] + "\\" + name
+                    shutil.copyfile(os.path.join(root, name), target)
+
+def clearTemp():
+    global tempDir
+    shutil.rmtree(tempDir)
+
+def getVersion():  
+    print ("Set version number:")
+    major = input("Major: ")
+    minor = input("Minor: ")
+    if len(minor) < 2:
+        minor = "0" + minor
+    patch = input("Patch: ")
+    if len(patch) < 2:
+        patch = "0" + patch
+    version = "{}v{}{}".format(major,minor,patch)
+    print ("Version: " + version)
+    return(version)
+
+def packPBO(version):
+    destDir = gitDir + "\\pbo\\" + version + "\\"
+    os.makedirs(destDir)
+    
+    for root, dirs, files in os.walk(mapsDir):
+        if root != mapsDir:
+            mapName = root[len(mapsDir) + 1:]
+            destFile = "[GrpW]_TvT128_xru_ZGM_{}.{}.pbo".format(version, mapName)
+
+            print("destFile: " + destFile)
+            print("destDir: " + destDir)
+
+            for file in files:
+                target = tempDir + "\\" + file
+                shutil.copyfile(os.path.join(root, file), tempDir + "\\" + file)
+
+            subprocess.run([
+                "C:\\Program Files\\PBO Manager v.1.4 beta\\PBOConsole.exe",
+                "-pack",
+                tempDir,
+                destDir + destFile
+            ], stdout = subprocess.PIPE)
+
+            os.remove(tempDir + "\\mission.sqm")
+            os.remove(tempDir + "\\default_loadouts.hpp")
+
+createTemp()
+version = getVersion()
+packPBO(version)
+clearTemp()
+print("Done!")
