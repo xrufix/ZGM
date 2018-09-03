92060af95e1079506c6118a13d3723788a56624b
rufix <stefan@ruhfass.com>
2018-09-03 15:45:08 +0200

    Spaces

--- xru_ZGM/loadouts/common/traits.hpp
+++ xru_ZGM/loadouts/common/traits.hpp
@@ -1,7 +1,7 @@
 
 #define SETENGINEER(int)   (_this select 0) setVariable [ARR_3(QQUOTE(ACE_isEngineer),int,true)];
-#define SETEOD(bool)        (_this select 0) setVariable [ARR_3(QQUOTE(ACE_isEOD),bool,true)];
-#define SETMEDIC(int)         (_this select 0) setVariable [ARR_3(QQUOTE(ACE_medical_medicClass), int, true)];
-#define SETPROTECTED(bool)     (_this select 0) setCaptive bool;
+#define SETEOD(bool)       (_this select 0) setVariable [ARR_3(QQUOTE(ACE_isEOD),bool,true)];
+#define SETMEDIC(int)      (_this select 0) setVariable [ARR_3(QQUOTE(ACE_medical_medicClass), int, true)];
+#define SETPROTECTED(bool) (_this select 0) setCaptive bool;
 
 #define TRAITS(medic,engineer,eod,protected) QUOTE(SETMEDIC(medic) SETENGINEER(engineer) SETEOD(eod) SETPROTECTED(protected))
